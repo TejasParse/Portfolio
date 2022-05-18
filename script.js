9abbba5cee15ef9eabbe4030f8689bfdb2bfdf9d
TejasParse <tejasparse1@gmail.com>
2022-05-18 19:14:39 +0530

    Contact Me Added

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function rotate(image) {
   image.setAttribute("style", "transform: rotate(" + rotateAngle + "deg)");
   rotateAngle = rotateAngle + 120;
 }
+
+// document.querySelector("#github").addEventListener("mouseover",()=>{
+//   console.log("Hover kiya apun");
+//   // document.querySelector("#github_contact").style.display = "inline"
+//   document.querySelector("#github_contact").style.opacity = 1
+// })
+
+// document.querySelector("#github").addEventListener("mouseleave",()=>{
+//   console.log("Hover nikala apun");
+//   // document.querySelector("#github_contact").style.display = "none"
+//   document.querySelector("#github_contact").style.opacity = 0
+// })
