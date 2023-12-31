ba3092c35ce337706374b785e909105a47ca8064
TejasParse <tejasparse1@gmail.com>
2024-01-01 00:48:10 +0530

    Updated Projects Page

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,20 +1,103 @@
 import React, { useState } from 'react';
 
+// import "./Projects.css"
+import ProjectsData from './Project.json';
+
+import { Button, Modal } from 'react-bootstrap';
+import { useSearchParams } from 'react-router-dom';
 import CodeforcesLogo from "./icons/codeforces.png";
 import Github from "./icons/github.png";
 import Linkedin from "./icons/linkedin.png";
 import WCA from "./icons/wca.svg";
 import Youtube from "./icons/youtube.png"
+import { logDOM } from '@testing-library/react';
 
+function MyVerticallyCenteredModal(props) {
+
+  // console.log(props.projectType, props.indexProject, ProjectsData, "Kya hua?");
+
+  const projectInformation = ProjectsData[props.projectType][props.indexProject];
+
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      className='modal-90w'
+      dialogClassName="modal-90w"
+      centered
+    >
+      <Modal.Header closeButton className='bg-slate-300'>
+        <Modal.Title id="contained-modal-title-vcenter">
+        <div className='py-2' dangerouslySetInnerHTML={{ __html: projectInformation.title }}></div>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className='bg-slate-200'>
+
+        <ul className='list-disc px-3'>
+          {projectInformation.description.map(elm=>{
+            return (
+              <li>{elm}</li>
+            )
+          })}
+        </ul>
+        <div className=' grid grid-cols-12 mt-2 bg-gray-300 py-2'>
+          {/* <div className='font-bold'>Tech Used: </div> */}
+          {
+            projectInformation?.techUsed?.map(elm=>{
+              return (
+                <div className={`col-span-6 flex flex-row justify-start items-center p-2`}>
+                  <img src={elm.url} className='rounded-full bg-slate-100 mx-2' width={"40px"}/>
+                  <div className='ms-1'>{elm.caption}</div>
+                </div>
+              )
+            })
+          }
+        </div>
+        
+
+        <div className='grid grid-cols-12 mt-2'>
+          {
+            projectInformation?.images?.map(elm=>{
+
+              return (
+                <div className={`col-span-${projectInformation.gridSpan || "12"} p-2 bg-gray-300`}>
+                  <img src={elm.url}/>
+                  <div className='text-center pt-2'>{elm.caption}</div>
+                </div>
+              )
+            })
+          }
+
+        </div>
+      </Modal.Body>
+      <Modal.Footer className='bg-slate-200'>
+        <button className='text-white bg-black p-2 py-1 rounded-md' onClick={props.onHide}>Close</button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 const Projects = () => {
 
-  const [projectType, setprojectType] = useState("Web");
+  const [searchParams, setSearchParams] = useSearchParams();
+  
+  let getType = searchParams.get("q");
+  console.log(getType, "Will it work now");
+
+  if(getType!=="Web" || getType!=="ML" || getType!=="App" || getType==null) {
+    getType="Web";
+  }
+
+  const [projectType, setprojectType] = useState(getType);
+  const [indexProject, setindexProject] = useState(0)
+
+  const [show, setShow] = useState(false);
 
   const selectedStyle = "bg-white border-2 border-black";
   const notSelectedStyle = "text-white";
 
-  const MlProjects = () => {
+  const MlProjectsOld = () => {
     return (
       <>
         <div className="border-x-4 border-x-brown-bg border-b-4 border-b-brown-bg px-2 mb-6">
@@ -94,7 +177,65 @@ const Projects = () => {
     );
   }
 
-  const WebProjects = () => {
+  const MlProjects = () => {
+
+    return (
+      <div className=''>
+        <div className='grid grid-cols-12 gap-3'>
+          {
+            ProjectsData["ML"].map((elm, index) => {
+              return (
+                <div className='col-span-6 border-2 border-black bg-light-brown-bg text-white text-center font-semibold'>
+                  <div >
+                    <div className='py-2 text-lg' dangerouslySetInnerHTML={{ __html: elm.title }}></div>
+                    <img src={elm.thumbnail} style={{ objectFit: "contain" }} />
+                    <div className='p-2 font-normal flex justify-between'>
+                      <div>
+                        {
+                          elm?.links?.map(lin => {
+                            return (
+                              <a
+                                href={lin.url}
+                                target="_blank"
+                              >
+                                <button className={`text-white bg-${lin.bg_color} p-2 py-1 rounded-md me-2`}>
+                                  {lin.text}
+                                </button>
+                              </a>
+                            )
+                          })
+                        }
+
+                      </div>
+                      <div>
+                        <button className="text-white bg-black p-2 py-1 rounded-md" onClick={() =>{ setShow(true); setindexProject(index) }}>
+                          Know More
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          }
+
+          <MyVerticallyCenteredModal
+            show={show}
+            onHide={() => setShow(false)}
+            projectType={projectType}
+            indexProject={indexProject}
+          />
+
+        </div>
+
+
+
+
+      </div>
+    )
+  }
+
+  const WebProjectsOld = () => {
     return (
       <>
         <div className="border-x-4 border-x-brown-bg border-b-4 border-b-brown-bg px-2 mb-6">
@@ -182,7 +323,7 @@ const Projects = () => {
                 Contributed to the <b>Winning Team</b> at <b>Smart India Hackathon 2023</b>, actively involved in developing an analytical portal for comparison of FPOs, coupled with a marketplace for income diversification.
               </li>
               <li>
-              Implemented a <b>Smart Voice-based Multilingual NLP Chatbot</b> using ReactJs (Frontend) and Nodejs (Backend), alongside Dialogflow for Text Chatbot, and MongoDB for efficient data management.
+                Implemented a <b>Smart Voice-based Multilingual NLP Chatbot</b> using ReactJs (Frontend) and Nodejs (Backend), alongside Dialogflow for Text Chatbot, and MongoDB for efficient data management.
               </li>
             </ul>
           </div>
@@ -226,6 +367,64 @@ const Projects = () => {
     );
   }
 
+  const WebProjects = () => {
+
+    return (
+      <div className=''>
+        <div className='grid grid-cols-12 gap-3'>
+          {
+            ProjectsData["Web"].map((elm, index) => {
+              return (
+                <div className='col-span-6 border-2 border-black bg-light-brown-bg text-white text-center font-semibold'>
+                  <div >
+                    <div className='py-2 text-lg' dangerouslySetInnerHTML={{ __html: elm.title }}></div>
+                    <img src={elm.thumbnail} style={{ objectFit: "contain" }} />
+                    <div className='p-2 font-normal flex justify-between'>
+                      <div>
+                        {
+                          elm?.links?.map(lin => {
+                            return (
+                              <a
+                                href={lin.url}
+                                target="_blank"
+                              >
+                                <button className={`text-white bg-${lin.bg_color} p-2 py-1 rounded-md me-2`}>
+                                  {lin.text}
+                                </button>
+                              </a>
+                            )
+                          })
+                        }
+
+                      </div>
+                      <div>
+                        <button className="text-white bg-black p-2 py-1 rounded-md" onClick={() =>{ setShow(true); setindexProject(index) }}>
+                          Know More
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          }
+
+          <MyVerticallyCenteredModal
+            show={show}
+            onHide={() => setShow(false)}
+            projectType={projectType}
+            indexProject={indexProject}
+          />
+
+        </div>
+
+
+
+
+      </div>
+    )
+  }
+
   return (
     <div className="h-screen flex flex-col justify-center">
       <div className="border-x-4 border-x-brown-bg border-b-4 border-b-brown-bg px-2 pb-3 mb-6">
@@ -234,7 +433,8 @@ const Projects = () => {
           <div className="bg-brown-bg px-2 rounded-lg flex flex-row justify-center content-center">
             <button className={`py-1 px-2 my-2 rounded-s-lg font-medium ${projectType == "Web" ? selectedStyle : notSelectedStyle}`} onClick={() => setprojectType("Web")}>Web</button>
             <button className={`py-1 px-2 my-2 font-medium ${projectType == "ML" ? selectedStyle : notSelectedStyle}`} onClick={() => setprojectType("ML")}>ML</button>
-            <button className={`py-1 px-2 my-2 rounded-e-lg font-medium ${projectType == "App" ? selectedStyle : notSelectedStyle}`} onClick={() => setprojectType("App")}>App</button>
+            <button className={`py-1 px-2 my-2 font-medium ${projectType == "App" ? selectedStyle : notSelectedStyle}`} onClick={() => setprojectType("App")}>App</button>
+            {/* <button className={`py-1 px-2 my-2 rounded-e-lg font-medium ${projectType == "Test" ? selectedStyle : notSelectedStyle}`} onClick={() => setprojectType("Test")}>Test</button> */}
           </div>
         </div>
       </div>
@@ -242,6 +442,7 @@ const Projects = () => {
       {projectType == "ML" && <MlProjects />}
       {projectType == "Web" && <WebProjects />}
       {projectType == "App" && <AppProjects />}
+      {/* {projectType == "Test" && <TestTemplates />} */}
 
     </div>
   );
