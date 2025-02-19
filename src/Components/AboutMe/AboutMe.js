6d956afdb8fb85f075cbd04bac7bfc3ae5abaed6
TejasParse <tejasparse1@gmail.com>
2025-02-19 02:00:30 -0700

    Updated Bio and Resume]

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -15,10 +15,16 @@ const AboutMe = (props) => {
       </h1>
       <div className="md:text-lg mb-3 md:mb-1">
 
-        I am a passionate Software Development and Machine Learning enthusiast with a B.Tech in Computer Science, specializing in AI & ML from Indian Institute of Information Technology Sri City, where I graduated with a CGPA of <b>9.30</b>.
-        My team, Git Clone, won the <b>Smart India Hackathon 2023</b>, hosted by the <b>Ministry of Education</b>.
-        Currently, I am pursuing a Master of Science in Computer Science at Arizona State University.
 
+        I am currently pursuing a <b>Master of Science</b> in <b>Computer Science</b> at <b>Arizona State University</b>,
+        maintaining a perfect GPA of <b>4.0</b>. I am proud to be one of the 1000s chosen to work part-time
+        as a Software Developer at Psych For Life, a startup under the Department of Psychology at ASU.
+        With over a <b>year and a half of software development experience</b>, I continue to refine my skills and
+        contribute to impactful projects.
+
+        {/* I am a passionate Software Development and Machine Learning enthusiast with a B.Tech in Computer Science, specializing in AI & ML from Indian Institute of Information Technology Sri City, where I graduated with a CGPA of <b>9.30</b>.
+        My team, Git Clone, won the <b>Smart India Hackathon 2023</b>, hosted by the <b>Ministry of Education</b>.
+        Currently, I am pursuing a Master of Science in Computer Science at Arizona State University. */}
         {/* I am a passionate Software Development and Machine Learning Enthusiast, who has completed my B.Tech in Computer Science with a specialization in AI & ML from the Indian Institute of Information Technology, Sri City, graduating with a CGPA of <b>9.30</b>. 
         My team, Git Clone, won the <b>Smart India Hackathon 2023</b> held by the <b>Ministry of Education</b>. 
         I have started my Master of Science in Computer Science at Arizona State University. */}
@@ -29,8 +35,12 @@ const AboutMe = (props) => {
         won the <b>Smart India Hackathon 2023</b> held by <b>Ministry of Education.</b>{" "} */}
       </div>
       <div className="md:text-lg mb-3 md:mb-1">
-        During my B.Tech, I worked on numerous projects in Web Development and Machine Learning and gained over a year of internship experience as a Web Developer,
-        particularly in the MERN stack. I am now seeking full-time opportunities in Machine Learning and Web Development to further broaden my expertise.
+        I completed my <b>Bachelor's in Computer Science and Engineering with a specialization in AI & ML</b> from the
+        <b> Indian Institute of Information Technology Sri City</b>, graduating with a GPA of <b>9.3/10</b>.
+        I have worked on numerous projects in Web Development and Machine Learning, and
+        <b> I am now actively seeking Summer Internship Opportunities in Software Development and Machine Learning.</b>
+        {/* During my B.Tech, I worked on numerous projects in Web Development and Machine Learning and gained over a year of internship experience as a Web Developer,
+        particularly in the MERN stack. I am now seeking full-time opportunities in Machine Learning and Web Development to further broaden my expertise. */}
         {/* During my B.Tech I have worked on several projects in Web Development
         and Machine Learning. I also have <b>over a year of Intern experience</b>{" "}
         as a Web Developer particularly in MERN stack. I am currently
